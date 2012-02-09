1bc24b2f954efda4f144da785598bd9af107d096
Kris Reynolds <krmreynolds@gmail.com>
2012-02-09 18:49:23 -0500

    Added additional functions to body, will change as need is determined

--- body.h
+++ body.h
@@ -8,6 +8,10 @@ class body {
     leg right_leg
     void set_mode(); 
     void set_speed();
+    void move_forward();
+    void move_backward();
+    void turn_left();
+    void turn_right();
   
  private:
     int mode;
