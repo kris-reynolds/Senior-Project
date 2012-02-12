86568dfa8816493f1bb06813d8bbc737007b6e13
Kris Reynolds <krmreynolds@gmail.com>
2012-02-11 21:25:47 -0500

    Added constants, fixed up

--- constants.h
+++ constants.h
@@ -1,9 +1,9 @@
-// Define direction of movement
+// Define direction key movement
 const int LEFT = 1;
 const int RIGHT = -1;
 
 const int STAIRS_MODE = 1;
 const int WALK_MODE = 2;
 const int HIGH_STEP_MODE = 3;
-const int TURN_RIGHT_MODE = 4;
-const int TURN_LEFT_MODE = 5;
+const int TURN_RIGHT = 4;
+const int TURN_LEFT = 5;
